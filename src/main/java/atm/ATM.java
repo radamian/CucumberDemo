32115e7bc79ca9da0ff807233a279e04ffc98e6e
Pavel R <rabeckijps@gmail.com>
2015-05-09 10:14:17 +0200

    step-4: fix ATM implementation to pass tests

--- src/main/java/atm/ATM.java
+++ src/main/java/atm/ATM.java
@@ -2,8 +2,19 @@ package atm;
 
 public class ATM {
 
-	public int withdraw(Account account, Integer amount){
-		account.withdraw(amount);
+	private String screen;
+
+	public String getScreen() {
+		return screen;
+	}
+
+	public int withdraw(Account account, Integer amount) {
+		try {
+			account.withdraw(amount);
+		} catch (Exception e) {
+			screen = e.getMessage();
+			amount = 0;
+		}
 		return amount;
 	}
 }
--- src/main/java/atm/Account.java
+++ src/main/java/atm/Account.java
@@ -8,7 +8,21 @@ public class Account {
 		this.balance = amount;
 	}
 
+	public Integer getBalance(){
+		return balance;
+	}
+
 	public void withdraw(Integer amount){
+		validateAmountToWithdraw(amount);
 		this.balance -= amount;
+	}
+
+	private void validateAmountToWithdraw(Integer amount) {
+		if(amount < 0){
+			throw new IllegalArgumentException("Cannot withdraw negative amount!");
+		}
+		if(balance < amount){
+			throw new IllegalArgumentException("Your account dont have enough money!");
+		}
 	}
 }
